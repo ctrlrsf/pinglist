3c2ce86bb4dfed5fd787037d3f4e639a294f71af
Rene Fragoso <ctrlrsf@gmail.com>
2015-02-26 11:07:16 -0500

    Add comments

--- host.go
+++ host.go
@@ -1,17 +1,23 @@
 package pinglist
 
+// Host holds information about a host that will be pinged, such as
+// IP address or hostname.
 type Host struct {
 	Address string
 }
 
+// HostRegistry keeps track of Hosts that will be pinged.
 type HostRegistry struct {
 	hostList []Host
 }
 
+// NewHostRegister returns new HostRegistry where hosts can later be added.
 func NewHostRegistry() *HostRegistry {
 	return &HostRegistry{}
 }
 
+// RegisterAddress creates a new Host with specified address and adds it
+// to the HostRegistry.
 func (hr *HostRegistry) RegisterAddress(address string) {
 	host := &Host{Address: address}
 
