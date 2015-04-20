33491052a00216b3e64b413a13c608337042abcc
Rene Fragoso <ctrlrsf@gmail.com>
2015-04-20 09:08:28 -0400

    Simplify in memory history log

--- history.go
+++ history.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"container/list"
 	"time"
 )
 
@@ -12,7 +11,7 @@ type LogEntry struct {
 }
 
 type HistoryLog struct {
-	Log map[string]*list.List
+	logEntries []LogEntry
 }
 
 // NewLogEntry creates and returns a new LogEntry object
@@ -20,33 +19,18 @@ func NewLogEntry(status HostStatus, latency time.Duration, timestamp time.Time) 
 	return LogEntry{status, latency, timestamp}
 }
 
-// NewHistoryLog creates a new HistoryLog and map of address to
-// log entries
-func NewHistoryLog() *HistoryLog {
+// NewHistoryLog creates a new HistoryLog struct with defaults
+func NewHistoryLog() HistoryLog {
 	hl := HistoryLog{}
-	hl.Log = make(map[string]*list.List)
-	return &hl
+	return hl
 }
 
 // AddLogEntry adds a log entry to history log for a host
-func (h *HistoryLog) AddLogEntry(address string, logEntry LogEntry) {
-	if _, ok := h.Log[address]; !ok {
-		h.Log[address] = list.New()
-	}
-
-	h.Log[address].PushBack(logEntry)
+func (h *HistoryLog) AddLogEntry(logEntry LogEntry) {
+	h.logEntries = append(h.logEntries, logEntry)
 }
 
-// GetLogEntryList returns the list of log entries for a host
-func (h *HistoryLog) GetLogEntryList(address string) *[]LogEntry {
-	leList := h.Log[address]
-	logEntrySlice := make([]LogEntry, leList.Len())
-
-	i := 0
-	for e := leList.Front(); e != nil; e = e.Next() {
-		logEntrySlice[i] = e.Value.(LogEntry)
-		i++
-	}
-
-	return &logEntrySlice
+// GetLogEntries returns the logEntries
+func (h *HistoryLog) GetLogEntries() []LogEntry {
+	return h.logEntries
 }
--- history_test.go
+++ history_test.go
@@ -5,41 +5,41 @@ import (
 	"time"
 )
 
-type TestHost struct {
+type FakeResult struct {
 	address string
 	status  HostStatus
 	latency time.Duration
 }
 
 func TestHistoryLog(t *testing.T) {
-	testHosts := []TestHost{
-		{"127.0.0.1", OnlineStatus, time.Second * 2},
-		{"127.0.0.2", OfflineStatus, time.Second * 1},
-		{"127.0.0.3", OfflineStatus, time.Second * 1},
-		{"127.0.0.4", OfflineStatus, time.Second * 1},
-		{"127.0.0.5", OfflineStatus, time.Second * 1},
+	fakeResults := []FakeResult{
+		{"127.0.0.1", Online, time.Second * 2},
+		{"127.0.0.1", Offline, time.Second * 1},
+		{"127.0.0.1", Offline, time.Second * 1},
+		{"127.0.0.1", Offline, time.Second * 1},
+		{"127.0.0.1", Offline, time.Second * 1},
 	}
 
 	hl := NewHistoryLog()
 
-	for i := range testHosts {
-		le := NewLogEntry(testHosts[i].status, testHosts[i].latency, time.Now())
-		hl.AddLogEntry(testHosts[i].address, le)
+	for i := range fakeResults {
+		le := NewLogEntry(fakeResults[i].status, fakeResults[i].latency, time.Now())
+		hl.AddLogEntry(le)
+	}
 
-		leList := hl.GetLogEntryList(testHosts[i].address)
-		if len(*leList) < 1 {
-			t.Errorf("Bad log entry list. Expected Len() >= 1, actual = %d\n", len(*leList))
-		}
+	logEntries := hl.GetLogEntries()
+	if len(logEntries) < 1 {
+		t.Errorf("Bad log entry list. Expected Len() >= 1, actual = %d\n", len(logEntries))
+	}
 
-		firstLe := (*leList)[0]
-		if firstLe.Status != testHosts[i].status {
-			t.Errorf("Bad log entry status. Expected = %d, actual = %d\n",
-				testHosts[i].status, firstLe.Status)
-		}
+	firstLe := logEntries[0]
+	if firstLe.Status != fakeResults[0].status {
+		t.Errorf("Bad log entry status. Expected = %d, actual = %d\n",
+			fakeResults[0].status, firstLe.Status)
+	}
 
-		if firstLe.Latency != testHosts[i].latency {
-			t.Error("Bad latency. Expected = %d, actual = %d\n",
-				testHosts[i].latency, firstLe.Latency)
-		}
+	if firstLe.Latency != fakeResults[0].latency {
+		t.Error("Bad latency. Expected = %d, actual = %d\n",
+			fakeResults[0].latency, firstLe.Latency)
 	}
 }
