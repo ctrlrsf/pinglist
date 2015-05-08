2883e6406999d1c8ee8bac58bb8277a2e666f54a
Rene Fragoso <ctrlrsf@gmail.com>
2015-05-08 14:55:36 -0400

    Initial support for saving a hosts to boltdb

--- bolt.go
+++ bolt.go
@@ -10,10 +10,25 @@ type BoltDbContext struct {
 	db *bolt.DB
 }
 
+// NewBoltDbContext creates a new BoltDbContext
 func (ctx *BoltDbContext) NewBoltDbContext(fileName string) {
 	var err error
 	ctx.db, err = bolt.Open(fileName, 0600, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+// Close closes an open boltdb file
+func (ctx *BoltDbContext) Close() {
+	ctx.db.Close()
+}
+
+// SaveHost saves a host in the boltdb file
+func (ctx *BoltDbContext) SaveHost(host Host) {
+	ctx.db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte("Hosts"))
+		err := b.Put([]byte(host.Address), []byte(host.Description))
+		return err
+	})
 }
